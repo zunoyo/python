fb686c76c3fd607a77de74c878a7246fcdc62121
zuno <domingo00uuu@gmail.com>
2023-03-28 12:53:15 +0900

    0328

---
+++ 0328/1.py
@@ -0,0 +1,46 @@
+
+
+
+'''
+str='Monty Python'
+len(str)
+print(len(str))
+
+print(str[0:5],str[6:],str[6:12])
+print(str[-12:-7],str[-6:],str[-6:0])
+
+'''
+'''
+print("ab"+"\b"+"c")
+print("hello\nworld")
+print("hello\vworld")
+print("aaaaa \'bbbbbb\'ccccccc")
+print("aaaaa \"bbbbbb\"ccccccc")
+'''
+
+'''
+str_var = "하하하하"
+str_var.replace("하","호")
+print(str_var.replace("하","호"))
+
+
+
+
+str_var2="안녕하세요 파이썬 파이썬 파이썬 파이썬 파이썬 파이썬 파이썬 파이썬  수업입니다."
+str_var3=str_var2.replace("파이썬","자바")
+str_var4=str_var2.replace("파이썬","자바",3)
+print(str_var2.replace("파이썬","자바"))
+print(str_var3)
+print(str_var4)
+'''
+
+#실수를 입력받음
+#102.345
+#1+0+2+3+4+5
+#sum 을 출력하라
+num_str=input("실수를 입력 하나 하세요")
+num_str.replace(".","") #102345
+sum=int(sum[0])+int(sum[1])+int(sum[2])+int(sum[3])+int(sum[4])+int(sum[5])
+print(sum)
+
+
